ef9ac6834e5cc7537188ba287d53f2dff080a8cd
lagoma619 <lagoma619@hotmail.com>
2024-04-02 00:35:42 -0500

    FORM 01

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/prueba', function () {
+    return view('content/prueba');
+});
 
 Auth::routes();
 
